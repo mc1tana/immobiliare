ecdadbc8f08c717f406723d1b7f6e16672c0fb07
michel cardon <michel-cardon@laposte>
2019-07-23 17:18:50 +0200

    m

--- index.php
+++ index.php
@@ -6,7 +6,46 @@
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
     </head>
     <body>
-        <h1>Bienvenue sur le site <?php bloginfo('name'); ?></h1>
-        <p><?php bloginfo('description'); ?></p>
+        
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+
+                    <a class="navbar-brand" href="#">
+                       
+                                 <?php bloginfo('name'); ?>
+                                
+
+
+                           
+                    
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Features</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pricing</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                        </ul>
+                    </div>
+             </div>   
+            
+        </nav>
+        <div class="container">
+                <h1 claas="text-center my-4">bienvenu sur le site<?php bloginfo('name'); ?> </h1>
+                <p><?php bloginfo('description') ?></p>
+        </div>
+
+        
     </body>
 </html>
